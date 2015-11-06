572fb649978b013d9fb43e23f2c00cf2d8fa3876
pxhanus <pxhanus@gmail.com>
2015-11-06 14:27:49 -0600

    Added comments

---
+++ NumberBreakdown/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPrompt = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lblDigits = New System.Windows.Forms.Label()
+        Me.btnBreakdown = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(35, 21)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(157, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Enter a number less than 1,000:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(198, 18)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(59, 20)
+        Me.txtNumber.TabIndex = 1
+        '
+        'lblDigits
+        '
+        Me.lblDigits.Location = New System.Drawing.Point(38, 41)
+        Me.lblDigits.Name = "lblDigits"
+        Me.lblDigits.Size = New System.Drawing.Size(219, 64)
+        Me.lblDigits.TabIndex = 2
+        Me.lblDigits.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnBreakdown
+        '
+        Me.btnBreakdown.Location = New System.Drawing.Point(103, 108)
+        Me.btnBreakdown.Name = "btnBreakdown"
+        Me.btnBreakdown.Size = New System.Drawing.Size(87, 28)
+        Me.btnBreakdown.TabIndex = 3
+        Me.btnBreakdown.Text = "Breakdown"
+        Me.btnBreakdown.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 148)
+        Me.Controls.Add(Me.btnBreakdown)
+        Me.Controls.Add(Me.lblDigits)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Name = "Form1"
+        Me.Text = "Number Breakdown"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPrompt As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblDigits As System.Windows.Forms.Label
+    Friend WithEvents btnBreakdown As System.Windows.Forms.Button
+
+End Class
---
+++ NumberBreakdown/Form1.vb
@@ -0,0 +1,58 @@
+﻿'Patricia Hanus
+'151106
+'Number Breakdown
+'______________________________________________________________________________
+Public Class Form1
+
+    Private Sub btnBreakdown_Click(sender As Object, e As EventArgs) Handles btnBreakdown.Click
+        Dim numberEntered As Integer
+        Dim onesDigit As Integer
+        Dim tensDigit As Integer
+        Dim hundredsDigit As Integer
+
+        numberEntered = Val(Me.txtNumber.Text)
+        If numberEntered < 10 Then
+            Me.lblDigits.Text = "The first digit is: " & numberEntered
+        ElseIf numberEntered < 100 Then
+            Call TwoDigits(numberEntered, tensDigit, onesDigit)
+            Me.lblDigits.Text = "The first digit is: " & tensDigit & vbCrLf & _
+                "The second digit is: " & onesDigit
+        ElseIf numberEntered < 1000 Then
+            Call ThreeDigits(numberEntered, hundredsDigit, tensDigit, onesDigit)
+            Me.lblDigits.Text = "The first digit is: " & hundredsDigit & vbCrLf & _
+                "The second digit is: " & tensDigit & vbCrLf & _
+                "The third digit is: " & onesDigit
+        Else
+            Me.lblDigits.Text = "Invalid entry."
+        End If
+    End Sub
+    '**************************************************************************
+    'The digits of a two-digit number are returned in separate parameters.
+    '
+    'pre:   num is a number less than 100 and greater than -100.
+    'post:  firstDigit stores a number between 0 and 9 inclusive.
+    'secondDigit stores a number between 0 and 9 inclusive.
+    '**************************************************************************
+    Sub TwoDigits(ByVal num As Integer, ByRef firstDigit As Integer, ByRef secondDigit As Integer)
+        firstDigit = num \ 10
+        secondDigit = num Mod 10
+    End Sub
+    '**************************************************************************
+    'The digits of a three-digit number are returned in separate parameters.
+    '
+    'pre:   num is a number less than 1000 and greater than -1000.
+    'post:  firstDigit stores a number between 0 and 9 inclusive.
+    '       secondDigit stores a number between 0 and 9 inclusive.
+    '       thirdDigit stores a number between 0 and 9 inclusive.
+    '**************************************************************************
+    Sub ThreeDigits(ByVal num As Integer, ByRef firstDigit As Integer, _
+    ByRef secondDigit As Integer, ByRef thirdDigit As Integer)
+        firstDigit = num \ 100
+        num = num Mod 100
+        Call TwoDigits(num, secondDigit, thirdDigit)
+    End Sub
+
+    Private Sub txtNumber_TextChanged(sender As Object, e As EventArgs) Handles txtNumber.TextChanged
+        Me.lblDigits.Text = Nothing
+    End Sub
+End Class
